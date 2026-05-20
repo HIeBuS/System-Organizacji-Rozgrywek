809e5cd4382f4d764d44a17c0598fa4fae326da4
Helii8 <2005dawid2005@gmail.com>
2026-05-21 00:06:28 +0200

    usuwanie druzyny

--- Liga.cpp
+++ Liga.cpp
@@ -6,9 +6,16 @@
 using namespace std;
 
 void Liga::dodajDruzyne(Druzyna d) {
+    listaDruzyn.push_back(d);
 }
 
 void Liga::usunDruzyne(string nazwa) {
+    for(auto it = listaDruzyn.begin(); it != listaDruzyn.end(); ++it){
+        if(it->pobierzNazwe() == nazwa){
+            listaDruzyn.erase(it);
+            break;
+        }
+    }
 }
 
 void Liga::generujTerminarz() {
